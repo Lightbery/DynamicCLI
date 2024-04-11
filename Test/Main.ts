5cd716ecf28f357e276b247fd6d25211f4ca27ac
LmanTW <82919705+LmanTW@users.noreply.github.com>
2024-04-11 17:17:13 +0800

    Delete Test directory

--- Test/Main.ts
+++
@@ -1,5 +0,0 @@
-import { DynamicCLI } from '../DynamicCLI.ts'
-
-new DynamicCLI()
-  .createPage('page1', 'Page 1', () => ['This is page 1'])
-  .createPage('page2', 'Page 2', () => ['This is page 2'])
